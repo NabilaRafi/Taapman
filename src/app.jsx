1281eb7570c34a6274f3cd9e29b2e5261d99f0a5
partham789 <partha.mukherjee1@wipro.com>
2016-08-05 16:54:02 +0100

    commit

--- src/app.jsx
+++ src/app.jsx
@@ -10,29 +10,30 @@ var selectedCityWeather;
 var currentCity = 0; // Index of current city displayed
 var currentCityName='London';
 
+
 var Weather = React.createClass({
     render: function() {
         
         //Build class names with dynamic data
     var weatherClass = classNames('wi wi-owm-' + this.state.weather);
     var bgColorClass = 'weather-widget '; // very-warm, warm, normal, cold, very-cold
-
-        // Set the background colour based on the temperature
-        if (this.state.temp >= 30) {
-        bgColorClass += 'very-warm';
-        }
-        else if (this.state.temp > 20 && this.state.temp < 30) {
-        bgColorClass += 'warm';
-        }
-        else if (this.state.temp > 10 && this.state.temp < 20) {
-        bgColorClass += 'normal';
-        }
-        else if (this.state.temp > 0 && this.state.temp < 10) {
-        bgColorClass += 'cold';
-        }
-        else if (this.state.temp <= 0) {
-        bgColorClass += 'very-cold';
-        }
+      bgColorClass += 'very-warm';
+//        // Set the background colour based on the temperature
+//        if (this.state.temp >= 30) {
+//        bgColorClass += 'very-warm';
+//        }
+//        else if (this.state.temp > 20 && this.state.temp < 30) {
+//        bgColorClass += 'warm';
+//        }
+//        else if (this.state.temp > 10 && this.state.temp < 20) {
+//        bgColorClass += 'normal';
+//        }
+//        else if (this.state.temp > 0 && this.state.temp < 10) {
+//        bgColorClass += 'cold';
+//        }
+//        else if (this.state.temp <= 0) {
+//        bgColorClass += 'very-cold';
+//        }
         
        var fixtures =[];
     // Render the DOM elements
@@ -51,11 +52,14 @@ var Weather = React.createClass({
                     <h1 className="city">{currentCityName}</h1>
                     <div className="weather">
                         <i className={weatherClass}></i>
+                        
                     </div>
                     <section className="weather-details">
-                        <div className="temp"><span className="temp-number">{this.state.temp}</span><span className="wi wi-degrees"></span>C</div>
+                        <div ><p>{this.state.description}</p></div>
+                        <div className="temp"><span className="temp-number">{this.state.temp}</span><span className="wi wi-degrees"></span>C</div>  
                         <div className="humidity"><i className="wi wi-raindrop"></i>{this.state.humidity} %</div>
-                        <div className="wind"><i className="wi wi-small-craft-advisory" >{this.state.wind}</i> <span className="vel">Km/h</span></div>
+                        <div className="wind"><i className="wi wi-small-craft-advisory" >{this.state.wind}</i> 
+                            <span className="vel">Km/h</span></div>
                     </section>
                 </div>
             </div>);
@@ -66,14 +70,13 @@ getInitialState: function() {
     return {
         weather: '',
         temp: 0,
+        description:'',
         humidity: 0,
         wind: 0,
         lat: 53.558572,
         lng : 9.9278215
     }  
 },
-    
-
     
 fetchData: function() {
     console.log('Fetch data');
@@ -84,6 +87,7 @@ fetchData: function() {
         Api.get({lat:this.state.lat,lng:this.state.lng})
             .then(function(data) {
                 selectedCityWeather = data;
+               //selectedCityWeather = data.description;
                 currentCityName=data.name;
                 this.updateData();
         }.bind(this));
@@ -92,9 +96,11 @@ fetchData: function() {
     
 updateData: function() {
     // Update the data for the UI
+    console.log(selectedCityWeather.weather[0].description);
     this.setState({
         weather: selectedCityWeather.weather[0].id,
         temp: Math.round(selectedCityWeather.main.temp - 273.15), // Kelvin to Celcius
+        description:selectedCityWeather.weather[0].description,
         humidity: Math.round(selectedCityWeather.main.humidity),
         wind: Math.round(selectedCityWeather.wind.speed),
         name:selectedCityWeather.name
